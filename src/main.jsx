e9f2a0f22880a84fdcadcc6def88450ef6f858b6
Ildi Dvorani <dvoranii@ildidev.com>
2024-07-30 22:42:53 -0400

    Add initial project files

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import App from "./App.jsx";
+
+const clientId = import.meta.env.VITE_CLIENT_ID;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId={clientId}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
+);
